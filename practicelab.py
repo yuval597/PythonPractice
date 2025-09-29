607dd4a6538fbeab20ccf6af9ffa2a6e2cf20cd9
Yuval Schreiber <yuvaliko597@gmail.com>
2025-09-29 13:17:39 +0300

    Added del practice

--- practicelab.py
+++ practicelab.py
@@ -28,3 +28,16 @@ if change_ip.lower() != 'no':
         server_ips[server_to_change] = new_ip
         print(f"IP address for {server_to_change} has been updated to {new_ip}.")
         print(f"Updated configuartions {server_ips}")
+
+del_server = input("Do you wanna delete any server? (yes/no)\n")
+if del_server.lower() == 'yes':
+    server_to_delete = input("Enter the server name you want to delete:\n")
+    if server_to_delete in server_ips:
+        del server_ips[server_to_delete]
+        print(f"{server_to_delete} has been deleted.")
+    else:
+        print("Server not found.")
+
+print("The updated configuration is:")
+for server, ip in server_ips.items():
+    print(f"{server} -> {ip}")
