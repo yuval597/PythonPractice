b96acbd464f38343511512546c4dfa73692ce74e
Yuval Schreiber <yuvaliko597@gmail.com>
2025-09-29 12:38:23 +0300

    Added first practice lab

---
+++ practicelab.py
@@ -0,0 +1,30 @@
+servers = []
+for i in range(3):
+    inputA = input('Enter server names \n')
+    servers.append(inputA)
+    
+print(f"The servers are = {servers}")
+
+server_ips = {}
+for server in servers:
+    ip = input(f"enter your ip adress for {server}\n")
+    server_ips[server] = ip 
+
+print("\nServer IP:")
+for server, ip in server_ips.items():
+    print(f"{server} : {ip}")
+
+var = input("Enter server name here\n")
+if var in server_ips:
+    print(f"The IP address of {var} is {server_ips[var]}")
+else:
+    print("Server not found")
+
+change_ip = input("Do you want to change any IP address? (yes/no)\n")
+if change_ip.lower() != 'no':
+    server_to_change = input("Enter the server name whose IP you want to change:\n")
+    if server_to_change in server_ips:
+        new_ip = input(f"Enter the new IP address for {server_to_change}:\n")
+        server_ips[server_to_change] = new_ip
+        print(f"IP address for {server_to_change} has been updated to {new_ip}.")
+        print(f"Updated configuartions {server_ips}")
